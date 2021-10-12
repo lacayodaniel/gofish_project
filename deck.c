b12f3191ad54baa4de89f833ea2d392ad5ea3479
Daniel Lacayo <daniellacayo@Daniels-Air.hsd1.ma.comcast.net>
2021-10-11 22:09:37 -0400

    part 1 creating the deck

--- deck.c
+++ deck.c
@@ -1,3 +1,22 @@
+
 #include "deck.h"
 
-
+int shuffle(){
+	char* SUITS[5] = {"H", "C", "S", "D"};
+	char* RANKS[14] = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+	struct card templist[52];
+	int i,j,n=0;
+	
+	for(i=0;i<4;i++){
+		for(j=0;j<13;j++){
+			struct card anon_card = {SUITS[i], RANKS[j]};
+			templist[n] = anon_card;
+			n++;
+		}
+	}
+	n--;
+	struct deck deck_instance = {templist, n};
+	
+	return 0;
+	
+}
--- gofish.c
+++ gofish.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "deck.h"
 
 int main(int args, char* argv[]) 
 {
-  fprintf(stdout, "Put your code here.");
+	fprintf(stdout, "Put your code here.\n");
+	fprintf(stdout, "called shuffled returned : %d\n", shuffle());
+	
+	struct deck *copy_deck;
+	copy_deck = &deck_instance;
+	printf("deck 0 %s\n", copy_deck->list);
+	
+	
+	return 0;
 }
+
+
